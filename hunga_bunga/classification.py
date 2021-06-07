16dfc13dc4f89d1feda1e9a008aa649048ce144b
shirshir05 <shirshir05@walla.co.il>
2021-06-07 10:01:37 +0300

    update param

--- hunga_bunga/classification.py
+++ hunga_bunga/classification.py
@@ -1,6 +1,6 @@
-
 import random
 import warnings
+
 warnings.filterwarnings('ignore')
 
 import numpy as np
@@ -30,7 +30,7 @@ from .core import *
 from .params import *
 import itertools as it
 
-#region models
+# region models
 linear_models_n_params = [
     (SGDClassifier,
      {'loss': ['log', 'modified_huber'],
@@ -39,7 +39,7 @@ linear_models_n_params = [
       }),
 
     (LogisticRegression,
-     {'penalty': penalty_12, 'max_iter': max_iter, 'tol': tol,  'warm_start': warm_start, 'C':C, 'solver': ['liblinear']
+     {'penalty': penalty_12, 'max_iter': max_iter, 'tol': tol, 'warm_start': warm_start, 'C': C, 'solver': ['liblinear']
       }),
 
     (Perceptron,
@@ -56,30 +56,32 @@ linear_models_n_params_small = linear_models_n_params
 
 svm_models_n_params = [
     (SVC,
-     {'C':C, 'kernel': kernel, 'degree': degree, 'gamma': gamma, 'coef0': coef0, 'shrinking': shrinking, 'tol': tol, 'max_iter': max_iter_inf2}),
+     {'C': C, 'kernel': kernel, 'degree': degree, 'gamma': gamma, 'coef0': coef0, 'shrinking': shrinking, 'tol': tol,
+      'max_iter': max_iter_inf2}),
 
     (NuSVC,
      {'nu': nu, 'kernel': kernel, 'degree': degree, 'gamma': gamma, 'coef0': coef0, 'shrinking': shrinking, 'tol': tol
       }),
 
     (LinearSVC,
-     { 'C': C, 'penalty_12': penalty_12, 'tol': tol, 'max_iter': max_iter,
-       'loss': ['hinge', 'squared_hinge'],
-       })
+     {'C': C, 'penalty_12': penalty_12, 'tol': tol, 'max_iter': max_iter,
+      'loss': ['hinge', 'squared_hinge'],
+      })
 ]
 
 svm_models_n_params_small = [
     (SVC,
-     {'C':C, 'kernel': kernel, 'degree': degree, 'gamma': gamma, 'coef0': coef0, 'shrinking': shrinking, 'tol': tol, 'max_iter': max_iter_inf2}),
+     {'C': C, 'kernel': kernel, 'degree': degree, 'gamma': gamma, 'coef0': coef0, 'shrinking': shrinking, 'tol': tol,
+      'max_iter': max_iter_inf2}),
 
     (NuSVC,
      {'nu': nu, 'kernel': kernel, 'degree': degree, 'gamma': gamma, 'coef0': coef0, 'shrinking': shrinking, 'tol': tol
       }),
 
     (LinearSVC,
-     { 'C': C, 'penalty': penalty_12, 'tol': tol, 'max_iter': max_iter,
-       'loss': ['hinge', 'squared_hinge'],
-       })
+     {'C': C, 'penalty': penalty_12, 'tol': tol, 'max_iter': max_iter,
+      'loss': ['hinge', 'squared_hinge'],
+      })
 ]
 
 neighbor_models_n_params = [
@@ -122,13 +124,13 @@ bayes_models_n_params = [
 
 nn_models_n_params_small = [
     (MLPClassifier,
-     { 'hidden_layer_sizes': [(64,), (32, 64)],
-       'batch_size': ['auto', 50],
-       'activation': ['identity', 'tanh', 'relu'],
-       'max_iter': [500],
-       'early_stopping': [True],
-       'learning_rate': learning_rate_small
-       })
+     {'hidden_layer_sizes': [(64,), (32, 64)],
+      'batch_size': ['auto', 50],
+      'activation': ['identity', 'tanh', 'relu'],
+      'max_iter': [500],
+      'early_stopping': [True],
+      'learning_rate': learning_rate_small
+      })
 ]
 
 tree_models_n_params = [
@@ -136,28 +138,33 @@ tree_models_n_params = [
     (RandomForestClassifier,
      {'criterion': ['gini', 'entropy'],
       'max_features': max_features, 'n_estimators': n_estimators, 'max_depth': max_depth,
-      'min_samples_split': min_samples_split, 'min_impurity_split': min_impurity_split, 'warm_start': warm_start, 'min_samples_leaf': min_samples_leaf,
+      'min_samples_split': min_samples_split, 'min_impurity_split': min_impurity_split, 'warm_start': warm_start,
+      'min_samples_leaf': min_samples_leaf,
       }),
 
     (DecisionTreeClassifier,
      {'criterion': ['gini', 'entropy'],
-      'max_features': max_features, 'max_depth': max_depth, 'min_samples_split': min_samples_split, 'min_impurity_split':min_impurity_split, 'min_samples_leaf': min_samples_leaf
+      'max_features': max_features, 'max_depth': max_depth, 'min_samples_split': min_samples_split,
+      'min_impurity_split': min_impurity_split, 'min_samples_leaf': min_samples_leaf
       }),
 
     (ExtraTreesClassifier,
      {'n_estimators': n_estimators, 'max_features': max_features, 'max_depth': max_depth,
-      'min_samples_split': min_samples_split, 'min_samples_leaf': min_samples_leaf, 'min_impurity_split': min_impurity_split, 'warm_start': warm_start,
+      'min_samples_split': min_samples_split, 'min_samples_leaf': min_samples_leaf,
+      'min_impurity_split': min_impurity_split, 'warm_start': warm_start,
       'criterion': ['gini', 'entropy']})
 ]
 
 tree_models_n_params_small = [
 
     (RandomForestClassifier,
-     {'max_features': max_features_small, 'n_estimators': n_estimators_small, 'min_samples_split': min_samples_split, 'max_depth': max_depth_small, 'min_samples_leaf': min_samples_leaf
+     {'max_features': max_features_small, 'n_estimators': n_estimators_small, 'min_samples_split': min_samples_split,
+      'max_depth': max_depth_small, 'min_samples_leaf': min_samples_leaf
       }),
 
     (DecisionTreeClassifier,
-     {'max_features': max_features_small, 'max_depth': max_depth_small, 'min_samples_split': min_samples_split, 'min_samples_leaf': min_samples_leaf
+     {'max_features': max_features_small, 'max_depth': max_depth_small, 'min_samples_split': min_samples_split,
+      'min_samples_leaf': min_samples_leaf
       }),
 
     (ExtraTreesClassifier,
@@ -166,7 +173,7 @@ tree_models_n_params_small = [
 ]
 
 best = [
-(RadiusNeighborsClassifier,
+    (RadiusNeighborsClassifier,
      {'radius': neighbor_radius, 'leaf_size': neighbor_leaf_size, 'metric': neighbor_metric,
       'weights': ['uniform', 'distance'],
       'p': [1, 2, 3, 4, 5],
@@ -180,37 +187,53 @@ best = [
       })
 ]
 
-#endregion
+# endregion
 
 
 nn_models_n_params = [
     (MLPClassifier,
      {'hidden_layer_sizes': [(512, 1024,)],
-       # 'activation': ['relu'],
-       #  'solver': ['adam'],
-       'alpha': alpha,  # L2 penalty (regularization term)
+      # 'activation': ['relu'],
+      #  'solver': ['adam'],
+
+      # 'alpha': alpha,  # L2 penalty (regularization term)
+      'alpha': [0.0001],
+
       # 'learning_rate': learning_rate, # Only used when solver='sgd',
       #   'learning_rate_init': [0.001],
       # 'tol': tol,
-      'warm_start': warm_start,
-       'batch_size': ['auto', 64, 32, 128],
-       'max_iter': [1000],
-       'early_stopping': [True, False],
-      'shuffle': [False, True]
 
-       })
+      # 'warm_start': warm_start,
+        'warm_start': [False],
+
+      # 'batch_size': ['auto', 64, 32, 128],
+    'batch_size': [128],
+
+      'max_iter': [1000],
+
+      # 'early_stopping': [True, False],
+      'early_stopping': [False],
+
+
+      # 'shuffle': [False, True],
+      'shuffle': [True],
+
+      'random_state': [1]
+
+      })
 ]
 
 
-
-def run_all_classifiers(x, y, small = False, normalize_x = False, n_jobs=cpu_count()-1, brain=False, test_size=0.2, n_splits=5, upsample=True, scoring=None, verbose=False, grid_search=False, ind=0):
+def run_all_classifiers(x, y, small=False, normalize_x=False, n_jobs=cpu_count() - 1, brain=False, test_size=0.2,
+                        n_splits=5, upsample=True, scoring=None, verbose=False, grid_search=False, ind=0):
     all_params = (nn_models_n_params_small if small else nn_models_n_params)
     # all_grid_params = dict(reduce(list.__add__, list(map(lambda x: list(x[1].items()), all_params)), []))
     # estimators = all_estimators()
     # estimators = [('MLP', MLPClassifier)]
 
     varNames = sorted(all_params[0][1])
-    combinations = [dict(zip(varNames, prod)) for prod in it.product(*(all_params[0][1][varName] for varName in varNames))]
+    combinations = [dict(zip(varNames, prod)) for prod in
+                    it.product(*(all_params[0][1][varName] for varName in varNames))]
     clf = MLPClassifier(**combinations[ind])
 
     # proba = []
@@ -235,16 +258,26 @@ def run_all_classifiers(x, y, small = False, normalize_x = False, n_jobs=cpu_cou
     # if ind:
     #     # all_params = [best[int(ind)]]
     #     all_params = [all_params[int(ind)]]
-    return combinations[ind], main_loop(clf, StandardScaler().fit_transform(x) if normalize_x else x, y, isClassification=True, n_jobs=n_jobs, verbose=verbose, brain=brain, test_size=test_size, n_splits=n_splits, upsample=upsample, scoring=scoring, grid_search=grid_search)
+    return combinations[ind], main_loop(clf, StandardScaler().fit_transform(x) if normalize_x else x, y,
+                                        isClassification=True, n_jobs=n_jobs, verbose=verbose, brain=brain,
+                                        test_size=test_size, n_splits=n_splits, upsample=upsample, scoring=scoring,
+                                        grid_search=grid_search)
 
-def run_one_classifier(x, y, small = True, normalize_x = True, n_jobs=cpu_count()-1, brain=False, test_size=0.2, n_splits=5, upsample=True, scoring=None, verbose=False, grid_search=True):
-    all_params = (linear_models_n_params_small if small else linear_models_n_params) +  (nn_models_n_params_small if small else nn_models_n_params) + ([] if small else gaussianprocess_models_n_params) + neighbor_models_n_params + (svm_models_n_params_small if small else svm_models_n_params) + (tree_models_n_params_small if small else tree_models_n_params)
+
+def run_one_classifier(x, y, small=True, normalize_x=True, n_jobs=cpu_count() - 1, brain=False, test_size=0.2,
+                       n_splits=5, upsample=True, scoring=None, verbose=False, grid_search=True):
+    all_params = (linear_models_n_params_small if small else linear_models_n_params) + (
+        nn_models_n_params_small if small else nn_models_n_params) + (
+                     [] if small else gaussianprocess_models_n_params) + neighbor_models_n_params + (
+                     svm_models_n_params_small if small else svm_models_n_params) + (
+                     tree_models_n_params_small if small else tree_models_n_params)
     all_params = random.choice(all_params)
     return all_params[0](**(list(ParameterSampler(all_params[1], n_iter=1))[0]))
 
 
 class HungaBungaClassifier(ClassifierMixin):
-    def __init__(self, brain=False, test_size = 0.2, n_splits = 5, random_state=None, upsample=True, scoring=None, verbose=False, normalize_x = True, n_jobs =cpu_count() - 1, grid_search=True, ind=None):
+    def __init__(self, brain=False, test_size=0.2, n_splits=5, random_state=None, upsample=True, scoring=None,
+                 verbose=False, normalize_x=True, n_jobs=cpu_count() - 1, grid_search=True, ind=None):
         self.model = None
         self.brain = brain
         self.test_size = test_size
@@ -262,7 +295,9 @@ class HungaBungaClassifier(ClassifierMixin):
         super(HungaBungaClassifier, self).__init__()
 
     def fit(self, x, y):
-        ans = run_all_classifiers(x, y, normalize_x=self.normalize_x, test_size=self.test_size, n_splits=self.n_splits, upsample=self.upsample, scoring=self.scoring, verbose=self.verbose, brain=self.brain, n_jobs=self.n_jobs, grid_search=self.grid_search, ind=self.ind)
+        ans = run_all_classifiers(x, y, normalize_x=self.normalize_x, test_size=self.test_size, n_splits=self.n_splits,
+                                  upsample=self.upsample, scoring=self.scoring, verbose=self.verbose, brain=self.brain,
+                                  n_jobs=self.n_jobs, grid_search=self.grid_search, ind=self.ind)
         self.combination, self.model, self.res = ans[0], ans[1][0], ans[1][1]
         return self
 
@@ -271,7 +306,8 @@ class HungaBungaClassifier(ClassifierMixin):
 
 
 class HungaBungaRandomClassifier(ClassifierMixin):
-    def __init__(self, brain=False, test_size = 0.2, n_splits = 5, random_state=None, upsample=True, scoring=None, verbose=False, normalize_x = True, n_jobs =cpu_count() - 1, grid_search=True):
+    def __init__(self, brain=False, test_size=0.2, n_splits=5, random_state=None, upsample=True, scoring=None,
+                 verbose=False, normalize_x=True, n_jobs=cpu_count() - 1, grid_search=True):
         self.model = None
         self.brain = brain
         self.test_size = test_size
@@ -286,7 +322,10 @@ class HungaBungaRandomClassifier(ClassifierMixin):
         super(HungaBungaRandomClassifier, self).__init__()
 
     def fit(self, x, y):
-        self.model = run_one_classifier(x, y, normalize_x=self.normalize_x, test_size=self.test_size, n_splits=self.n_splits, upsample=self.upsample, scoring=self.scoring, verbose=self.verbose, brain=self.brain, n_jobs=self.n_jobs, grid_search=self.grid_search)
+        self.model = run_one_classifier(x, y, normalize_x=self.normalize_x, test_size=self.test_size,
+                                        n_splits=self.n_splits, upsample=self.upsample, scoring=self.scoring,
+                                        verbose=self.verbose, brain=self.brain, n_jobs=self.n_jobs,
+                                        grid_search=self.grid_search)
         self.model.fit(x, y)
         return self
 
@@ -300,4 +339,3 @@ if __name__ == '__main__':
     clf = HungaBungaClassifier()
     clf.fit(X, y)
     print(clf.predict(X).shape)
-
